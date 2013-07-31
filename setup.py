2207feda1624ad7c64674dc6cd8859a18b003848
Peter Hudec <peterhudec@peterhudec.com>
2013-07-31 19:04:53 +0200

    Changed the package name.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup,find_packages
 
 setup(
-    name='Test Live Server',
+    name='TestLiveServer',
     version='0.0.0',
     packages=find_packages(),
     package_data={'': ['*.txt', '*.rst']},
