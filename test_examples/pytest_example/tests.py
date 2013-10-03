b1a24a26aeb34c9ce9fa3e271fef4634b35f35b5
Peter Hudec <peterhudec@peterhudec.com>
2013-10-03 17:43:17 +0200

    Test now check also the app name.

--- test_examples/pytest_example/tests.py
+++ test_examples/pytest_example/tests.py
@@ -4,7 +4,6 @@ An example of testing a Flask app with py.test and Selenium with help of testliv
 """
 
 from os import path
-
 from selenium import webdriver
 import pytest
 import testliveserver as tls
@@ -14,19 +13,26 @@ def abspath(pth):
     return path.join(path.dirname(__file__), '../..', pth)
 
 
+PORT = 8001
+
+
 APPS = {
     'Pyramid': tls.WsgirefSimpleServer(
         abspath('sample_apps/pyramid/main.py'),
-        port=5000
+        port=PORT
     ),
     'Flask': tls.Flask(
         abspath('sample_apps/flask/main.py'),
-        port=5000
+        port=PORT
     ),
     'GAE': tls.GAE(
         abspath('venv/bin/google_appengine/dev_appserver.py'),
         abspath('sample_apps/gae'),
-        port=8080
+        port=PORT
+    ),
+    'Django': tls.Django(
+        abspath('sample_apps/django/example'),
+        port=PORT
     ),
 }
 
@@ -34,6 +40,7 @@ APPS = {
 @pytest.fixture('module', APPS)
 def app(request):
     app = APPS[request.param]
+    app.name = request.param
     
     try:
         # Run the live server.
@@ -48,15 +55,20 @@ def app(request):
 
 @pytest.fixture('module')
 def browser(request):
-    browser = webdriver.Chrome()
+    
+    tls.port_in_use(PORT, True)
+    
+    browser = webdriver.PhantomJS()
     browser.implicitly_wait(3)
+    
     request.addfinalizer(lambda: browser.quit())
     return browser
 
 
 def test_home(browser, app):
     """Andy visits a webpage and sees "Home"."""
-    
-    browser.get(app.url)
-    page_text = browser.find_element_by_tag_name('body').text
-    assert 'Home' in page_text
+        
+    browser.get(app.url)    
+    page_text = browser.find_element_by_tag_name('body').text    
+    assert 'Home {}'.format(app.name) in page_text
+
