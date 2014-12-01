edc9f915814c7988dd23ac8b6f8e66b231bd500d
Peter Hudec <peterhudec@peterhudec.com>
2014-12-01 23:19:02 +0100

    Removed blank lines.

--- sample_apps/flask/main.py
+++ sample_apps/flask/main.py
@@ -18,9 +18,7 @@ def home():
 
 
 if __name__ == '__main__':
-    
     # This does nothing unles you run this module with --liveandletdie flag.
     import liveandletdie
     liveandletdie.Flask.wrap(app)
-    
     app.run()
